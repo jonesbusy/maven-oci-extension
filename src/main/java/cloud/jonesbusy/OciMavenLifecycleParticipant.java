82d5c6ca9d1b1d813db979e341ea03cb989a391d
Valentin Delaye <jonesbusy@gmail.com>
2024-09-24 10:31:18 +0200

    Update plugin to work with Java 17

--- src/main/java/cloud/jonesbusy/OciMavenLifecycleParticipant.java
+++ src/main/java/cloud/jonesbusy/OciMavenLifecycleParticipant.java
@@ -3,18 +3,12 @@ package cloud.jonesbusy;
 import org.apache.maven.AbstractMavenLifecycleParticipant;
 import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
-import org.slf4j.Logger;
-
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Named("oci")
 @Singleton
 public class OciMavenLifecycleParticipant extends AbstractMavenLifecycleParticipant {
-
-    @Inject
-    private Logger log;
 
     @Override
     public void afterSessionStart(MavenSession session)throws MavenExecutionException {
